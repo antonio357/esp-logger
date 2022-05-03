a2606d8f3e28a15adc61d712d3660ec53eea3d22
antonio357 <carlosalbuquerque359@gmail.com>
2022-05-03 20:48:31 -0300

    reformat

--- Esp8266LogSimulation/src/main.cpp
+++ Esp8266LogSimulation/src/main.cpp
@@ -1,49 +1,27 @@
 #include <ESP8266WiFi.h>
-
 #include <WiFiClient.h>
-
 #include <ESP8266WebServer.h>
 
 const char *ssid = "test";
-
 const char *password = "password";
 
 ESP8266WebServer server(80);
 
 void handleRoot() {
-
-server.send(200, "text/html", "<h1>You are connected</h1>");
-
+  server.send(200, "text/plain", "you are connect to esp8266");
 }
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Configuring access point…");
+  WiFi.softAP(ssid, password);
 
-delay(1000);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.printf("AP IP address: "); Serial.println(myIP);
 
-Serial.begin(9600);
-
-Serial.println();
-
-Serial.print("Configuring access point…");
-
-WiFi.softAP(ssid, password);
-
-IPAddress myIP = WiFi.softAPIP();
-
-Serial.print("AP IP address: ");
-
-Serial.println(myIP);
-
-server.on("/", handleRoot);
-
-server.begin();
-
-Serial.println("HTTP server started");
-
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
-void loop() {
-
-server.handleClient();
-
-}
+void loop() {server.handleClient();}
