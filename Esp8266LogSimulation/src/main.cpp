8612340a524786fbd34cdedcf74bf2edc3c97c3e
antonio357 <carlosalbuquerque359@gmail.com>
2022-05-03 20:35:10 -0300

    server inside esp wifi acess point

--- Esp8266LogSimulation/src/main.cpp
+++ Esp8266LogSimulation/src/main.cpp
@@ -1,19 +1,49 @@
-
-// create acess point
-// https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/soft-access-point-examples.html
 #include <ESP8266WiFi.h>
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println();
+#include <WiFiClient.h>
 
-  Serial.print("Setting soft-AP ... ");
-  boolean result = WiFi.softAP("my-esp", "password");
-  if(result == true) Serial.println("Ready");
-  else Serial.println("Failed!");
+#include <ESP8266WebServer.h>
+
+const char *ssid = "test";
+
+const char *password = "password";
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+
+server.send(200, "text/html", "<h1>You are connected</h1>");
+
+}
+
+void setup() {
+
+delay(1000);
+
+Serial.begin(9600);
+
+Serial.println();
+
+Serial.print("Configuring access point…");
+
+WiFi.softAP(ssid, password);
+
+IPAddress myIP = WiFi.softAPIP();
+
+Serial.print("AP IP address: ");
+
+Serial.println(myIP);
+
+server.on("/", handleRoot);
+
+server.begin();
+
+Serial.println("HTTP server started");
+
 }
 
 void loop() {
-  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
-  delay(3000);
+
+server.handleClient();
+
 }
