a6532ed749e75f019a3bb8f5e89091511f08acb2
antonio357 <carlosalbuquerque359@gmail.com>
2022-05-08 00:45:09 -0300

    recebendo request post

--- Esp8266LogSimulation/src/main.cpp
+++ Esp8266LogSimulation/src/main.cpp
@@ -1,5 +1,5 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+// #include <ESP8266WiFi.h>
+// #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
 const char *ssid = "test";
@@ -7,9 +7,20 @@ const char *password = "password";
 
 ESP8266WebServer server(80);
 
-void handleRoot() {
+void home() {
   server.send(200, "text/plain", "you are connect to esp8266");
 }
+
+void handleBody() { 
+  Serial.println(server.arg("plain"));
+  Serial.println(server.args());
+}
+
+void start() {
+  server.send(200, "text/plain", "it will start send logs in 3 s");
+  handleBody();
+}
+
 
 void setup() {
   Serial.begin(9600);
