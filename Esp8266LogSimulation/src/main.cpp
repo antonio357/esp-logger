f91feb67f08e407d643f45d462de8c4b4e9fb6fc
antonio357 <carlosalbuquerque359@gmail.com>
2022-05-10 11:33:44 -0300

    node code

--- Esp8266LogSimulation/src/main.cpp
+++ Esp8266LogSimulation/src/main.cpp
@@ -1,60 +1,78 @@
-// #include <ESP8266WiFi.h>
-// #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>
 
 const char *ssid = "test";
 const char *password = "password";
 
-ESP8266WebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
-void home() {
-  server.send(200, "text/plain", "you are connect to esp8266");
+// Tratamento de eventos dos dados que vêm do cliente 
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
+
+  switch (type) {
+    case WStype_DISCONNECTED:
+      break;
+
+    case WStype_CONNECTED:
+      { IPAddress ip = webSocket.remoteIP(num);
+        Serial.println(ip);
+      }
+      break;
+
+    case WStype_TEXT:
+      { String text = String((char *) &payload[0]);
+        Serial.println(text);
+        Serial.println(num);
+        Serial.println(type);
+
+        if (text == "ON") {
+          digitalWrite(BUILTIN_LED, LOW);
+        } else {
+          digitalWrite(BUILTIN_LED, HIGH);
+        }
+      }
+      break;
+
+  }
+
 }
 
-String getBody() { 
-  return server.arg("plain");
+void socketReciveCommands() {
+
 }
 
-void start() {
-  const String body = getBody();
-  Serial.println(body);
-  server.send(200, "application/json", body);
+void socketsendLogs() {
+  
 }
-
-void sendPost() {
-  delay(4000);
-  HTTPClient http;    //Declare object of class HTTPClient
-  WiFiClient wifiClient;
- 
-  http.begin(wifiClient, "192.168.4.2", 8080, "/log");      //Specify request destination
-  http.addHeader("Content-Type", "text/plain");  //Specify content-type header
- 
-  int httpCode = http.POST("Message from ESP8266");   //Send the request
-  String payload = http.getString();                  //Get the response payload
- 
-  Serial.println(httpCode);   //Print HTTP return code
-  Serial.println(payload);    //Print request response payload
- 
-  http.end();  //Close connection
-}
-
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Configuring access point…");
   WiFi.softAP(ssid, password);
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.printf("AP IP address: "); Serial.println(myIP);
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH);
 
-  server.on("/", HTTP_GET, home);
-  server.on("/start", HTTP_PATCH, start);
-  server.begin();
-  Serial.println("HTTP server started");
+  Serial.println(WiFi.softAPIP());
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
 }
 
 void loop() {
-  server.handleClient();
-  sendPost();
+  // sending data
+  delay(3000);
+  webSocket.sendTXT(0, "digitalRead(BUILTIN_LED)");
 }
+
+// node code remove this later
+// Coloque aqui o IP obtino no ESP8266
+// let con = WebSocket.server('ws://192.168.4.1:81/', ['arduino']);
+let ws = new WebSocket("ws://192.168.4.1:81/");
+
+ws.onopen = function (event) {
+  ws.send("node client found");
+};
+
+ws.onmessage = function (evt) { 
+  console.log("esp send ", evt.data);
+};
