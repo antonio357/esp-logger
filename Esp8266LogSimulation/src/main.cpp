d3036ae89c96358594606df2ca3c0d1508a2d19b
antonio357 <carlosalbuquerque359@gmail.com>
2022-07-26 13:56:15 -0300

    sent a json string

--- Esp8266LogSimulation/src/main.cpp
+++ Esp8266LogSimulation/src/main.cpp
@@ -1,5 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsServer.h>
+#include <ArduinoJson.h> 
+
+// https://www.youtube.com/watch?v=15X0WvGaVg8&ab_channel=mo_thunderz
+// The JSON library uses static memory, so this will need to be allocated:
+StaticJsonDocument<200> doc_tx;                       // provision memory for about 200 characters
+StaticJsonDocument<200> doc_rx;
 
 boolean send_log = false;
 
@@ -31,16 +37,43 @@ void connectToRouter(const char *ssid, const char *password) {
 }
 
 void receiveMsg(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {  
-  // make soure any operation is made if (type == WStype_TEXT) the type is correct
-  if (type == WStype_TEXT) {
-    char payloadString[strlen((char*)(payload))];
-    
-    strcpy(payloadString, (char*)(payload));
-    if (strcmp(payloadString, "start logs") == 0) send_log = true;
-    else if (strcmp(payloadString, "stop logs") == 0) send_log = false;
+  switch (type) {                                     // switch on the type of information sent
+    case WStype_DISCONNECTED:                         // if a client is disconnected, then type == WStype_DISCONNECTED
+      Serial.println("Client " + String(num) + " disconnected");
+      break;
+    case WStype_CONNECTED:                            // if a client is connected, then type == WStype_CONNECTED
+      Serial.println("Client " + String(num) + " connected");
+      break;
+    case WStype_TEXT:                                 // if a client has sent data, then type == WStype_TEXT
+      char payloadString[strlen((char*)(payload))];
+      
+      strcpy(payloadString, (char*)(payload));
+      if (strcmp(payloadString, "start logs") == 0) send_log = true;
+      else if (strcmp(payloadString, "stop logs") == 0) send_log = false;
 
-    Serial.printf("received: payload [%u]: %s\n", num, payload);
-  }
+      Serial.printf("received: payload [%u]: %s\n", num, payload);
+      // try to decipher the JSON string received
+      // DeserializationError error = deserializeJson(doc_rx, payload);
+      // if (error) {
+      //   Serial.print(F("deserializeJson() failed: "));
+      //   Serial.println(error.f_str());
+      //   return;
+      // }
+      // else {
+      //   // JSON string was received correctly, so information can be retrieved:
+      //   const char* g_brand = doc_rx["brand"];
+      //   const char* g_type = doc_rx["type"];
+      //   const int g_year = doc_rx["year"];
+      //   const char* g_color = doc_rx["color"];
+      //   Serial.println("Received guitar info from user: " + String(num));
+      //   Serial.println("Brand: " + String(g_brand));
+      //   Serial.println("Type: " + String(g_type));
+      //   Serial.println("Year: " + String(g_year));
+      //   Serial.println("Color: " + String(g_color));
+      // }
+      // Serial.println("");
+      break;
+  };
 }
 
 void setup() {
@@ -57,7 +90,13 @@ void loop() {
   webSocket.loop();
   const char msg[] = "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc";
   if (send_log && webSocket.connectedClients()) {
-    webSocket.broadcastTXT(msg, strlen(msg));
+    // webSocket.broadcastTXT(msg, strlen(msg));
+    String jsonString = "";                           // create a JSON string for sending data to the client
+    JsonObject object = doc_tx.to<JsonObject>();      // create a JSON Object
+    object["rand1"] = random(100);                    // write data into the JSON object -> I used "rand1" and "rand2" here, but you can use anything else
+    object["rand2"] = random(100);
+    serializeJson(doc_tx, jsonString);                // convert JSON object to string
+    webSocket.broadcastTXT(jsonString);               // send JSON string to clients
     counter++;
   } else if (counter > 0) {
     Serial.print("sent msgs: ");
